61965175fad9f8881a65371ee37169c227c22809
Justin Wingo <justin.wingo@mailchimp.com>
2021-06-14 19:51:48 -0400

    Please make the changes to all your tests

--- test/dressesTabTest.js
+++ test/dressesTabTest.js
@@ -4,7 +4,7 @@ import searchresults from '../pages/SearchResultsPage';
 
 const URL = 'http://automationpractice.com/index.php';
 const getURL = ClientFunction(() => window.location.href);
-const dressTab = Selector('#block_top_menu > ul > li:nth-child(2) > a');
+const dressTab = Selector('#block_top_menu > ul > li:nth-child(2) > a'); //<= this should not be in the test file. you should have a page object that has all the selectors for that page
 
 fixture("Getting started")
   .page(URL);
@@ -13,5 +13,5 @@ fixture("Getting started")
     await t
     .expect(getURL()).eql(URL)
     .click(dressTab)
-    .expect(getURL()).contains('category');
+    .expect(getURL()).contains('category'); // <= this is not really a valid assertion. each of the tabs have the same text at the end of the url
   });
