c4111f2b6cc3f1d5c70e1cbd3855ec5bd9fdd971
Anthony Mwangi <amwangi@rsglab.com>
2021-06-28 23:28:16 -0400

    coorect changes for createaccount

--- test/createAccountTest.js
+++ test/createAccountTest.js
@@ -3,11 +3,6 @@ import HomePage from '../pages/HomePage';
 import LoginPage from '../pages/LoginPage';
 import CreateAccountPage from '../pages/CreateAccountPage';
 import RegisterPage from '../pages/RegisterPage';
-<<<<<<< HEAD
-//import {orderData} from '../data/data_helper';
-=======
-import {orderData} from '../data/data_helper';
->>>>>>> 63b1496d98764d867b609057291f193cd201ef03
 import {envVariables} from '../config/config';
 
 var randomNumber = Math.floor(Math.random() * 10000);
@@ -23,40 +18,9 @@ fixture("Creating new account")
         .click(HomePage.signInLink)
         .typeText(CreateAccountPage.EmailInput,email)
         .click(CreateAccountPage.CreatAccountLink)
-<<<<<<< HEAD
     //Enter New User Info
         await RegisterPage.userInfo()
     // New User Login
         await LoginPage.logIn();
-=======
-    
-    //Enter info for new registration
-        .click(RegisterPage.genderOption)
-        .typeText(RegisterPage.firstName,orderData.firstname)
-        .typeText(RegisterPage.lastName,orderData.lastname)
-        .typeText(RegisterPage.Password,orderData.password)
-        .click(RegisterPage.daysSelect)
-        .click(RegisterPage.daysSelect.find('option').withText('10'))
-        .click(RegisterPage.monthsSelect)
-        .click(RegisterPage.monthsSelect).find('option').withText('August')
-        .click(RegisterPage.yearsSelect)
-        .click(RegisterPage.yearsSelect.find('option').withText('1979'))
-        .typeText(RegisterPage.Address,orderData.address)
-        .typeText(RegisterPage.City,orderData.city)
-        .click(RegisterPage.stateDropDown)
-        .click(RegisterPage.stateDropDown.find('option').withText('Georgia'))
-        .typeText(RegisterPage.zipCode,orderData.zipcode)
-        .click(RegisterPage.addressCountry.withText('United States'))
-        .typeText(RegisterPage.mobilePhone,orderData.phone)
-        .click(RegisterPage.registerButton)
-    //Logout
-        .click(RegisterPage.signOut)
-    //Login
-        .click(RegisterPage.signIn)
-        .expect(LoginPage.AccountHeader.exists).ok()
-        .typeText(LoginPage.EmailInput,email)
-        .typeText(LoginPage.PasswordInput,orderData.password)
-        .click(LoginPage.SignInButton);   
->>>>>>> 63b1496d98764d867b609057291f193cd201ef03
 
     });
